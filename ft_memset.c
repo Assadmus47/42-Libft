7b7ec80f024b677d6bed7f9301d531bb788d46d0
assadMus47 <assadkac47@gmail.com>
2025-11-12 18:34:56 +0100

    fixing strlen and finishing memset

--- ft_memset.c
+++ ft_memset.c
@@ -10,3 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void * ft_memset( void * pointer, int value, size_t count )
+{
+	size_t	i;
+	unsigned char	*p;
+
+	i = 0;
+	p = (unsigned char *)pointer;
+	while (i < count)
+	{
+		p[i++] = value;
+	}
+	return (pointer);
+}
